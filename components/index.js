199cabe9314c0dce4e0b103b1a3c4a2e81f3634f
Roger Rajaratnam <roger@sourcier.uk>
2017-05-18 10:07:39 +0100

    Make componenets importable without direct refference

---
+++ components/index.js
@@ -0,0 +1,3 @@
+export {default as FormInputGroup} from './FormInputGroup/FormInputGroup'
+export {default as FormInput} from './FormInput/FormInput'
+export {default as Header} from './Header/Header'
---
+++ dist/index.js
@@ -0,0 +1,34 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _FormInputGroup = require('./FormInputGroup/FormInputGroup');
+
+Object.defineProperty(exports, 'FormInputGroup', {
+  enumerable: true,
+  get: function get() {
+    return _interopRequireDefault(_FormInputGroup).default;
+  }
+});
+
+var _FormInput = require('./FormInput/FormInput');
+
+Object.defineProperty(exports, 'FormInput', {
+  enumerable: true,
+  get: function get() {
+    return _interopRequireDefault(_FormInput).default;
+  }
+});
+
+var _Header = require('./Header/Header');
+
+Object.defineProperty(exports, 'Header', {
+  enumerable: true,
+  get: function get() {
+    return _interopRequireDefault(_Header).default;
+  }
+});
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
