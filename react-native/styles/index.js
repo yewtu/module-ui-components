d75d786a2c70350e65664cc48841b1f129a6d222
Jonny Wyatt <jonnywyatt@yahoo.co.uk>
2017-06-09 15:56:25 +0100

    Add colours, spacing styles for react native

--- react-native/styles/index.js
+++ react-native/styles/index.js
@@ -2,11 +2,13 @@ import {StyleSheet} from 'react-native';
 import typography from './typography';
 import backgrounds from './backgrounds';
 import layout from './layout';
+import spacing from './spacing';
 
 export const globalStyles = {
-  ...backgrounds,
-  ...layout,
-  ...typography
+	...backgrounds,
+	...layout,
+	...typography,
+	...spacing
 };
 
 export default StyleSheet.create(globalStyles)
---
+++ react-native/styles/spacing.js
@@ -0,0 +1,14 @@
+export default {
+	paddingSm: {
+		paddingTop: 10,
+		paddingLeft: 10,
+		paddingRight: 10,
+		paddingBottom: 10,
+	},
+	paddingMl: {
+		paddingTop: 20,
+		paddingLeft: 20,
+		paddingRight: 20,
+		paddingBottom: 20,
+	}
+}
