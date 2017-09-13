f2a46228d96121c67d6b9d3a54266c4a5e5b6211
Jonny Wyatt <jonnywyatt@yahoo.co.uk>
2017-09-13 16:15:08 +0100

    FormInputGroup - inlineLabel option

--- components/FormInputGroup/FormInputGroup.jsx
+++ components/FormInputGroup/FormInputGroup.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import FormInput from '../FormInput/FormInput';
 
-const FormInputGroup = ({id, label, helpText, input, error, showErrorMsg, isInline, suffixLabel, size}) => {
+const FormInputGroup = ({id, label, helpText, input, error, showErrorMsg, isInline, inlineLabel, suffixLabel, size}) => {
   return (<div className={`form-input-group${isInline ? ' form-input-group--inline': ''}${error ? ' form-input-group--has-error': ''} ${size ? ' form-input-group--' + size : '' }`}>
-    <div><label className="form-control-label" htmlFor={id}>{label}</label></div>
+    <label className={`form-control-label ${inlineLabel ? 'form-control-label--inline' : ''}`} htmlFor={id}>{label}</label>
     { helpText && <div className="form-control-help">{helpText}</div> }
     { error && showErrorMsg && <div className="form-control-error">{error}</div> }
     <FormInput id={id} hasError={Boolean(error)} size={size} {...input}/>
@@ -22,7 +22,8 @@ FormInputGroup.propTypes = {
 };
 
 FormInputGroup.defaultProps = {
-	showErrorMsg: true
+	showErrorMsg: true,
+  inlineLabel: false
 };
 
 export default FormInputGroup;
