e1df2c64c7d5e8ebc42bd8de9ebceb0ff9624da0
Jonny Wyatt <jonnywyatt@yahoo.co.uk>
2017-09-15 14:59:58 +0100

    Icon button padding / min width / stories

--- components/Button/stories.js
+++ components/Button/stories.js
@@ -10,3 +10,11 @@ storiesOf('Buttons/ButtonLink', module)
 	.add('Secondary', withInfo('')(() =>
 		<ButtonLink linkUrl='test' type='secondary'>Enter more data</ButtonLink>
 	));
+
+storiesOf('Buttons/Icon buttons', module)
+	.add('Icon with text', withInfo('')(() =>
+		<button className='btn btn--icon btn--primary'><i className='icon icon--white icon--sm icon--plus' />Add</button>
+	))
+	.add('Icon only, no background', withInfo('')(() =>
+		<button className='btn btn--icon-only'><i className='icon icon--gray icon--plus' /></button>
+	));
