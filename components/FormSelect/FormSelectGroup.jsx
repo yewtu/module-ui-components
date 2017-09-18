b72e69cf2bb695b03e58a6dbbafad19090978fc6
Jonny Wyatt <jonnywyatt@yahoo.co.uk>
2017-09-18 16:26:28 +0100

    FormSelect - showLabel prop

--- components/FormSelect/FormSelectGroup.jsx
+++ components/FormSelect/FormSelectGroup.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const FormSelectGroup = ({id, label, helpText, select, hasError, isInline, size='md'}) => {
+const FormSelectGroup = ({id, label, helpText, select, hasError, isInline, size, showLabel}) => {
   return (<div className={`form-input-group${isInline ? ' form-input-group--inline': ''}`}>
-    <label className="form-control-label" htmlFor={id}>{label}</label>
+    <label className={`form-control-label${showLabel ? '' : ' sr-only'}`} htmlFor={id}>{label}</label>
     { helpText && <div className="form-control-help">{helpText}</div> }
     <select id={id}
             name={select.name}
             onChange={select.onChange}
             value={select.value}
-            className={`form-select ${select.classNames || ''} ${hasError ? ' form-select--has-error' : ''} ${size ? ' form-select--' + size: '' }`}
+            className={`form-select ${select.classNames || ''} ${hasError ? ' form-select--has-error' : ''} ${size ? ' form-select--' + size: '' } ${select.autoWidth ? ' form-select--inline' : ''}`}
     >
 			{
 				select.options.map((option, idx) =>
@@ -26,6 +26,7 @@ FormSelectGroup.propTypes = {
   label: PropTypes.string,
   helpText: PropTypes.string,
   hasError: PropTypes.bool,
+  showLabel: PropTypes.bool,
   select: PropTypes.shape({
     name: PropTypes.string,
     classNames: PropTypes.string,
@@ -36,7 +37,13 @@ FormSelectGroup.propTypes = {
     focus: PropTypes.bool,
 		optionLabelKey: PropTypes.string,
 		optionValueKey: PropTypes.string,
+		autoWidth: PropTypes.bool
   }),
+};
+
+FormSelectGroup.defaultProps = {
+  size: 'md',
+  showLabel: true
 };
 
 export default FormSelectGroup;
