ff26dbddc4ba5740efafac9a97b9623403b2988a
Jonny Wyatt <jonnywyatt@yahoo.co.uk>
2017-09-18 07:59:03 +0100

    Tweak size variant for button, collapsible, input

--- components/Collapsible/Collapsible.jsx
+++ components/Collapsible/Collapsible.jsx
@@ -19,8 +19,8 @@ export default class Collapsible extends React.Component {
 		return (
 			<div className="collapsible">
 				<button className={`collapsible__trigger btn${this.state.collapsed ? '' : ' active'}`} type="button" onClick={this.onToggle}>
-					<span className="icon-text icon-text--right">
-						<span className={triggerTextClasses}>{triggerTitle}</span>
+					<span className={`icon-text icon-text--right icon-text--${triggerSize}`}>
+						<span className={`collapsible__trigger-title ${triggerTextClasses}`}>{triggerTitle}</span>
 						<i className={`icon icon--chevron-down icon--gray icon--${triggerSize}`}></i>
 						<i className={`icon icon--chevron-up icon--gray icon--${triggerSize}`}></i>
 					</span>
