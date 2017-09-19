e57d22fb05c2c59161beb16c98b7cb7d76f6f620
Jonny Wyatt <jonnywyatt@yahoo.co.uk>
2017-09-19 12:31:31 +0100

    FormInputGroup - fix inline variant; add story

--- components/FormInputGroup/stories.js
+++ components/FormInputGroup/stories.js
@@ -4,22 +4,49 @@ import {storiesOf} from '@storybook/react';
 
 storiesOf('Forms/FormInputGroup', module)
 	.add('Label above', () => (
-		<FormInputGroup label='EID tag'/>
+		<FormInputGroup label='EID tag' id='eidTag' input={{name: 'eidTag'}}/>
 	))
 	.add('Label inline', () => (
-		<FormInputGroup label='EID tag' inlineLabel={true}/>
+		<FormInputGroup label='EID tag' id='eidTag' inlineLabel={true} input={{name: 'eidTag'}}/>
+	))
+	.add('Multiple inputs for number range (inline)', () => (
+		<div>
+			<p>If it's for a range, use 'min' and 'max' to prevent the second input value being less than the first, and vice
+				versa.</p>
+			<form action=''>
+			<span className="padding-right-xs">
+				<FormInputGroup
+					label='From'
+					id='from'
+					isInline={true}
+					input={{name: 'from', type: 'number', value: 10, max: 20, step: 1}}/>
+			</span>
+				<FormInputGroup
+					label='To'
+					id='to'
+					isInline={true}
+					input={{name: 'to', type: 'number', value: 20, min: 10, step: 1}}/>
+				<div className='margin-top-md'>
+					<button type='submit' className='btn btn--primary btn--sm'>Submit</button>
+				</div>
+			</form>
+		</div>
 	))
 	.add('With help text', () => (
-		<FormInputGroup label='Reserve price' helpText='Based on current market prices we suggest £3.55'/>
+		<FormInputGroup id='reserve' label='Reserve price' helpText='Based on current market prices we suggest £3.55'
+										input={{name: 'reserve'}}/>
 	))
 	.add('With error message', () => (
-		<FormInputGroup label='Live weight' error='Must be greater than 100Kg'/>
+		<FormInputGroup id='liveWeight' label='Live weight' error='Must be greater than 100Kg'
+										input={{name: 'liveWeight'}}/>
 	))
 	.add('Currency input', () => (
 		<FormInputGroup
 			label='Live weight'
 			suffixLabel='per Kg'
+			id='liveWeight'
 			input={{
+				name: 'liveWeight',
 				prefixLabel: '£',
 				classNames: 'form-input--auto-width',
 				type: 'number',
@@ -31,23 +58,45 @@ storiesOf('Forms/FormInputGroup', module)
 				pattern: '\d{1,4}',
 			}}/>
 	))
-	.add('Date input', () => (
-		<FormInputGroup
-			label='Date reading was taken'
-			input={{
-				name: 'processingDate',
-				classNames: 'form-input--auto-width',
-				type: 'date',
-				required: true,
-				value: ''
-			}}/>
+	.add('Single date input', () => (
+		<div>
+			<FormInputGroup
+				label='Date reading was taken'
+				id='processingDate'
+				input={{
+					name: 'processingDate',
+					classNames: 'form-input--auto-width',
+					type: 'date',
+					required: true,
+					value: ''
+				}}/>
+		</div>
+	))
+	.add('Date range', () => (
+		<div>
+			<p>If it's for a date range, use 'min' and 'max' to prevent the end date being before the start date</p>
+			<span className="padding-right-xs">
+				<FormInputGroup
+					label='Start date'
+					id='startDate'
+					isInline={true}
+					input={{name: 'startDate', type: 'date', value: '2017-09-01', max: '2017-09-10'}}/>
+			</span>
+			<FormInputGroup
+				label='End date'
+				id='endDate'
+				isInline={true}
+				input={{name: 'endDate', type: 'date', value: '2017-09-10', min: '2017-09-01'}}/>
+		</div>
 	))
 	.add('Small size', () => (
 		<FormInputGroup
 			label='Live weight'
 			suffixLabel='per Kg'
+			id='liveWeight'
 			size='sm'
 			input={{
+				name: 'liveWeight',
 				prefixLabel: '£',
 				classNames: 'form-input--auto-width',
 				type: 'number',
@@ -60,6 +109,6 @@ storiesOf('Forms/FormInputGroup', module)
 			}}/>
 	))
 	.add('Extra small size', () => (
-		<FormInputGroup label='EID tag' size='xs'/>
+		<FormInputGroup id='eidTag' label='EID tag' size='xs' input={{name: 'eidTag'}}/>
 	));
 
--- dist/FormInputGroup/stories.js
+++ dist/FormInputGroup/stories.js
@@ -13,18 +13,59 @@ var _react3 = require('@storybook/react');
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 (0, _react3.storiesOf)('Forms/FormInputGroup', module).add('Label above', function () {
-	return _react2.default.createElement(_FormInputGroup2.default, { label: 'EID tag' });
+	return _react2.default.createElement(_FormInputGroup2.default, { label: 'EID tag', id: 'eidTag', input: { name: 'eidTag' } });
 }).add('Label inline', function () {
-	return _react2.default.createElement(_FormInputGroup2.default, { label: 'EID tag', inlineLabel: true });
+	return _react2.default.createElement(_FormInputGroup2.default, { label: 'EID tag', id: 'eidTag', inlineLabel: true, input: { name: 'eidTag' } });
+}).add('Multiple inputs for number range (inline)', function () {
+	return _react2.default.createElement(
+		'div',
+		null,
+		_react2.default.createElement(
+			'p',
+			null,
+			'If it\'s for a range, use \'min\' and \'max\' to prevent the second input value being less than the first, and vice versa.'
+		),
+		_react2.default.createElement(
+			'form',
+			{ action: '' },
+			_react2.default.createElement(
+				'span',
+				{ className: 'padding-right-xs' },
+				_react2.default.createElement(_FormInputGroup2.default, {
+					label: 'From',
+					id: 'from',
+					isInline: true,
+					input: { name: 'from', type: 'number', value: 10, max: 20, step: 1 } })
+			),
+			_react2.default.createElement(_FormInputGroup2.default, {
+				label: 'To',
+				id: 'to',
+				isInline: true,
+				input: { name: 'to', type: 'number', value: 20, min: 10, step: 1 } }),
+			_react2.default.createElement(
+				'div',
+				{ className: 'margin-top-md' },
+				_react2.default.createElement(
+					'button',
+					{ type: 'submit', className: 'btn btn--primary btn--sm' },
+					'Submit'
+				)
+			)
+		)
+	);
 }).add('With help text', function () {
-	return _react2.default.createElement(_FormInputGroup2.default, { label: 'Reserve price', helpText: 'Based on current market prices we suggest \xA33.55' });
+	return _react2.default.createElement(_FormInputGroup2.default, { id: 'reserve', label: 'Reserve price', helpText: 'Based on current market prices we suggest \xA33.55',
+		input: { name: 'reserve' } });
 }).add('With error message', function () {
-	return _react2.default.createElement(_FormInputGroup2.default, { label: 'Live weight', error: 'Must be greater than 100Kg' });
+	return _react2.default.createElement(_FormInputGroup2.default, { id: 'liveWeight', label: 'Live weight', error: 'Must be greater than 100Kg',
+		input: { name: 'liveWeight' } });
 }).add('Currency input', function () {
 	return _react2.default.createElement(_FormInputGroup2.default, {
 		label: 'Live weight',
 		suffixLabel: 'per Kg',
+		id: 'liveWeight',
 		input: {
+			name: 'liveWeight',
 			prefixLabel: '£',
 			classNames: 'form-input--auto-width',
 			type: 'number',
@@ -35,22 +76,53 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 			placeholder: '0',
 			pattern: '\d{1,4}'
 		} });
-}).add('Date input', function () {
-	return _react2.default.createElement(_FormInputGroup2.default, {
-		label: 'Date reading was taken',
-		input: {
-			name: 'processingDate',
-			classNames: 'form-input--auto-width',
-			type: 'date',
-			required: true,
-			value: ''
-		} });
+}).add('Single date input', function () {
+	return _react2.default.createElement(
+		'div',
+		null,
+		_react2.default.createElement(_FormInputGroup2.default, {
+			label: 'Date reading was taken',
+			id: 'processingDate',
+			input: {
+				name: 'processingDate',
+				classNames: 'form-input--auto-width',
+				type: 'date',
+				required: true,
+				value: ''
+			} })
+	);
+}).add('Date range', function () {
+	return _react2.default.createElement(
+		'div',
+		null,
+		_react2.default.createElement(
+			'p',
+			null,
+			'If it\'s for a date range, use \'min\' and \'max\' to prevent the end date being before the start date'
+		),
+		_react2.default.createElement(
+			'span',
+			{ className: 'padding-right-xs' },
+			_react2.default.createElement(_FormInputGroup2.default, {
+				label: 'Start date',
+				id: 'startDate',
+				isInline: true,
+				input: { name: 'startDate', type: 'date', value: '2017-09-01', max: '2017-09-10' } })
+		),
+		_react2.default.createElement(_FormInputGroup2.default, {
+			label: 'End date',
+			id: 'endDate',
+			isInline: true,
+			input: { name: 'endDate', type: 'date', value: '2017-09-10', min: '2017-09-01' } })
+	);
 }).add('Small size', function () {
 	return _react2.default.createElement(_FormInputGroup2.default, {
 		label: 'Live weight',
 		suffixLabel: 'per Kg',
+		id: 'liveWeight',
 		size: 'sm',
 		input: {
+			name: 'liveWeight',
 			prefixLabel: '£',
 			classNames: 'form-input--auto-width',
 			type: 'number',
@@ -62,5 +134,5 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 			pattern: '\d{1,4}'
 		} });
 }).add('Extra small size', function () {
-	return _react2.default.createElement(_FormInputGroup2.default, { label: 'EID tag', size: 'xs' });
+	return _react2.default.createElement(_FormInputGroup2.default, { id: 'eidTag', label: 'EID tag', size: 'xs', input: { name: 'eidTag' } });
 });
